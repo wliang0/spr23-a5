568fb7ecefc67600959a7a67b47967f0c229b8f4
Wilson Liang <wilsonliang@wilsons-air-2.lan>
2023-05-20 14:59:42 -0700

    add Makefile and tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,6 +3,10 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
+# Checks division 10 / 5 = 2
+assert run("10 / 5").output == "2"
+# Checks subtraction 3 - 1 = 2
+assert run("3 - 1").output == "2"
 
 
 ###
